98dfb4a33290e3eb1f001bcf27148b0ceb957eff
Luis Carrion Castañeda <luiscastaneda019@gmail.com>
2025-08-27 16:19:42 -0500

    third_commit

--- conexion.php
+++ conexion.php
@@ -2,11 +2,11 @@
 
 	function conexion(){
 
-	$host = "dpg-d2nmbljipnbc738cen5g-a.oregon-postgres.render.com";
-	$port = "5432";
-	$dbname = "exampledbnube2_iayu";
-	$user = "exampledbnube2_iayu_user";
-	$password = "grgcmNohJpxOCjwAx11xd1DfLqhlef4n";
+	$host = "host=dpg-d2nmbljipnbc738cen5g-a.oregon-postgres.render.com";
+	$port = "port=5432";
+	$dbname = "dbname=exampledbnube2_iayu";
+	$user = "user=exampledbnube2_iayu_user";
+	$password = "password=grgcmNohJpxOCjwAx11xd1DfLqhlef4n";
 
 	$db = pg_connect("$host $port $dbname $user $password");
 
